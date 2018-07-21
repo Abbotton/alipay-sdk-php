9fcc19df9aca6d0937c7c3ff6bf531c12820e0c3
wi1dcard <wi1dcard@qq.com>
2018-07-22 01:05:05 +0800

    完善 AlipayResponseException

--- aop/Exception/AlipayResponseException.php
+++ aop/Exception/AlipayResponseException.php
@@ -9,5 +9,13 @@ class AlipayResponseException extends AlipayException
     public function __construct($response, $externalMessage = '')
     {
         $errorResponse = $response->{static::ERROR_NODE};
+        if(is_object($errorResponse)) {
+            $msg = $externalMessage == '' ? '' : $externalMessage . ': ';
+            $msg .= isset($errorResponse->msg) ? $errorResponse->msg : '';
+            $msg .= isset($errorResponse->sub_msg) ? " ($errorResponse->sub_msg)" : '';
+
+            $code = isset($errorResponse->code) ? $errorResponse->code : 0;
+            parent::__construct($msg, $code);
+        }
     }
 }
