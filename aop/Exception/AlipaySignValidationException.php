487ae8f12258a6c6357a777842305bd47c396597
wi1dcard <wi1dcard@qq.com>
2018-07-22 21:21:02 +0800

    完善 SignValidationException

--- aop/Exception/AlipaySignValidationException.php
+++ aop/Exception/AlipaySignValidationException.php
@@ -4,8 +4,10 @@ namespace Alipay\Exception;
 
 class AlipaySignValidationException extends AlipayException
 {
-    public function __construct($exceptSign, $data)
+    public function __construct($sign, $data, $externalMessage = '')
     {
-        parent::__construct("Sign: {$exceptSign}, Data: {$data}");
+        $message = $externalMessage == '' ? '' : $externalMessage . ': ';
+        $message .= "Sign = {$sign}, Data = {$data}";
+        parent::__construct($message);
     }
 }
