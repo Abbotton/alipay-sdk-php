a29ba95721e246863734d14280ee6168678e46b3
wi1dcard <wi1dcard@qq.com>
2018-07-22 15:47:15 +0800

    完善 response exception

--- aop/Exception/AlipayResponseException.php
+++ aop/Exception/AlipayResponseException.php
@@ -8,14 +8,24 @@ class AlipayResponseException extends AlipayException
 
     public function __construct($response, $externalMessage = '')
     {
-        $errorResponse = $response->{static::ERROR_NODE};
-        if (is_object($errorResponse)) {
-            $msg = $externalMessage == '' ? '' : $externalMessage . ': ';
-            $msg .= isset($errorResponse->msg) ? $errorResponse->msg : '';
-            $msg .= isset($errorResponse->sub_msg) ? " ($errorResponse->sub_msg)" : '';
-
-            $code = isset($errorResponse->code) ? $errorResponse->code : 0;
-            parent::__construct($msg, $code);
+        if (is_string($response)) {
+            $response = json_decode($response);
         }
+        if (!is_object($response) || !isset($response->{static::ERROR_NODE})) {
+            $message = $externalMessage;
+            $code = 0;
+        } else {
+            $errorResponse = $response->{static::ERROR_NODE};
+            $message = $externalMessage == '' ? '' : $externalMessage . ': ';
+            if (is_object($errorResponse)) {
+                $message .= isset($errorResponse->msg) ? $errorResponse->msg : '';
+                $message .= isset($errorResponse->sub_msg) ? " ($errorResponse->sub_msg)" : '';
+                $code = isset($errorResponse->code) ? $errorResponse->code : 0;
+            } else {
+                $message .= $errorResponse;
+                $code = 0;
+            }
+        }
+        parent::__construct($message, $code);
     }
 }
