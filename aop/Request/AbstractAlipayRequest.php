54f1668bede6bd07702f777da16c3b8634248079
wi1dcard <wi1dcard@qq.com>
2018-07-23 09:04:36 +0800

    前面漏掉的提交

--- aop/Request/AbstractAlipayRequest.php
+++ aop/Request/AbstractAlipayRequest.php
@@ -2,6 +2,9 @@
 
 namespace Alipay\Request;
 
+use Alipay\Exception\AlipayException;
+use Alipay\Exception\AlipayInvalidPropertyException;
+
 abstract class AbstractAlipayRequest
 {
     public static function className()
@@ -9,7 +12,14 @@ abstract class AbstractAlipayRequest
         return __CLASS__;
     }
 
-    abstract public function getApiMethodName();
+    public function getApiMethodName()
+    {
+        $name = static::className();
+        $name = preg_replace('/([A-Z])/s','.$1', $name);
+        $name = trim($name);
+        $name = strtolower($name);
+        return $name;
+    }
 
     public function getApiVersion()
     {
@@ -27,4 +37,57 @@ abstract class AbstractAlipayRequest
     abstract public function getProdCode();
     
     abstract public function getReturnUrl();
+
+    public function __construct($config = [])
+    {
+        foreach($config as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public function __get($name)
+    {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter();
+        } elseif (method_exists($this, 'set' . $name)) {
+            throw new AlipayInvalidPropertyException('Getting write-only property', $name);
+        }
+        throw new AlipayInvalidPropertyException('Getting unknown property', $name);
+    }
+
+    public function __set($name, $value)
+    {
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter($value);
+        } elseif (method_exists($this, 'get' . $name)) {
+            throw new AlipayInvalidPropertyException('Setting read-only property', $name);
+        } else {
+            throw new AlipayInvalidPropertyException('Setting unknown property', $name);
+        }
+    }
+
+    public function __isset($name)
+    {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter() !== null;
+        }
+        return false;
+    }
+
+    public function __unset($name)
+    {
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter(null);
+        } elseif (method_exists($this, 'get' . $name)) {
+            throw new AlipayInvalidPropertyException('Unsetting read-only property', $name);
+        }
+    }
 }
