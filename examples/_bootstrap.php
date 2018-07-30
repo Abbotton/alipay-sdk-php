21203567374395a32dfcd490058ff46ef805d4e3
wi1dcard <wi1dcard@qq.com>
2018-07-30 17:52:01 +0800

    删除一处无用的 use

--- examples/_bootstrap.php
+++ examples/_bootstrap.php
@@ -3,7 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Alipay\AlipayKeyPair;
-use Alipay\AlipaySigner;
 use Alipay\AopClient;
 
 ini_set('html_errors', '0');
@@ -12,7 +11,5 @@ ini_set('html_errors', '0');
 
 return new AopClient(
     getenv('APP_ID'),
-    new AlipaySigner(
-        AlipayKeyPair::create(getenv('APP_PRIV_KEY'), getenv('ALIPAY_PUB_KEY'))
-    )
+    AlipayKeyPair::create(getenv('APP_PRIV_KEY'), getenv('ALIPAY_PUB_KEY'))
 );
