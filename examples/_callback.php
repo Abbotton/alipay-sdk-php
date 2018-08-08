cb5793c91d4d2145d8cbc5a081c41ad37a6dfae1
wi1dcard <wi1dcard@qq.com>
2018-08-08 17:15:07 +0800

    更新支付回调例子

--- examples/_callback.php
+++ examples/_callback.php
@@ -7,21 +7,15 @@ $aop = require __DIR__ . '/_bootstrap.php';
 
 ob_start(); // 启动输出缓冲区
 
-// 判断是否为控制台环境运行此脚本
-if (php_sapi_name() === 'cli') {
-    parse_str($argv[1], $params); // 使用输入参数解析为数组作为参数
-} else {
-    $params = $_POST; // 直接使用 $_POST 作为参数
-}
-
 // ---业务代码开始---
 try {
-    $signer = $aop->getSigner(); // 获取签名器，使用 `verifyByParams` 验证签名
-    $key = $aop->getKeyPair()->getPublicKey(); // 获取支付宝公钥，用于验证签名
-    $signer->verifyByParams( 
-        $params, // 支付宝服务器发来的参数数组
-        $key->asResource()
-    );
+    // 判断是否为控制台环境运行此脚本
+    if (php_sapi_name() === 'cli') {
+        parse_str($argv[1], $params); // 使用输入参数解析为数组作为参数
+        $aop->verify($params);
+    } else {
+        $aop->verify(); // 直接使用 $_POST 作为参数
+    }
     print_r($params); // 验证签名成功，数据未被篡改且可靠，打印参数
 } catch (\Exception $ex) {
     // 验证签名失败或发生错误，打印异常信息
