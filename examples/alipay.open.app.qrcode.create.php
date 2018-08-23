2980f40cf9092adad5195783bd2a91bc1d541fcd
wi1dcard <wi1dcard@qq.com>
2018-08-23 10:07:29 +0800

    新增生成小程序分享二维码的例子

---
+++ examples/alipay.open.app.qrcode.create.php
@@ -0,0 +1,32 @@
+<?php
+
+use Alipay\AlipayRequestFactory;
+
+$aop = require __DIR__ . '/_bootstrap.php';
+
+/**
+ * 根据文件名剥离请求名
+ */
+$apiName = basename(__FILE__, '.php');
+
+/**
+ * 构建请求类
+ */
+$request = AlipayRequestFactory::create($apiName, [
+    'biz_content' => [
+        'url_param' => '/',
+        'query_param' => 'foo=bar',
+        'describe' => 'baz',
+    ],
+]);
+
+/**
+ * 发起请求并调试输出结果
+ */
+try {
+    $data = $aop->execute($request)->getData();
+    print_r($data);
+} catch (\Exception $ex) {
+    http_response_code(500);
+    print_r($ex);
+}
