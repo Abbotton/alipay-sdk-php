acbd1fb3e48f0c959fb2762769bf7ca379d286a7
wi1dcard <wi1dcard@qq.com>
2018-08-28 15:21:50 +0800

    尝试解决 passphrase 问题

--- aop/Key/AlipayPrivateKey.php
+++ aop/Key/AlipayPrivateKey.php
@@ -6,7 +6,7 @@ class AlipayPrivateKey extends AlipayKey
 {
     public static function toString($resource, $configargs = [])
     {
-        return openssl_pkey_export($resource, $key, '', $configargs) ? $key : parent::toString($resource);
+        return openssl_pkey_export($resource, $key, null, $configargs) ? $key : parent::toString($resource);
     }
 
     public static function getKey($certificate)
